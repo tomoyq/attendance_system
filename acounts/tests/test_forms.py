fc25f89e2dbf1e35708008c1b096ed61b0a71b61
tomoya <arata5.18830@gmail.com>
2024-10-16 18:07:16 +0900

    formのテスト実装#3

--- acounts/tests/test_forms.py
+++ acounts/tests/test_forms.py
@@ -7,12 +7,13 @@ class Test_LoginForm(TestCase):
 
     def setUp(self):
         self.manager = Manager.objects.create(name="田中太郎")
-        CustomUser.objects.create(employee_number=111111, name="田中太郎", password='test_pass' , manager_id=self.manager, )
+        #createメソッドだとパスワードがハッシュ化されないためis_validメソッドが常にfalseを返すため
+        CustomUser.objects.create_user(employee_number=111111, name="田中太郎", password='test_pass' , manager_id=self.manager, )
 
     #社員番号が数字ではない場合エラー
     def test_no_number(self):
         form_data = {
-            '社員番号' : '111111',
+            "employee_number" : "社員番号",
             'password' : 'test_pass'
         }
         form = LoginForm(data=form_data)
@@ -22,7 +23,7 @@ class Test_LoginForm(TestCase):
     #社員番号の桁数が少ない場合エラー
     def test_different_digits(self):
         form_data = {
-            '社員番号' : 11,
+            "employee_number" : 11,
             'password' : 'test_pass'
         }
         form = LoginForm(data=form_data)
@@ -32,7 +33,7 @@ class Test_LoginForm(TestCase):
     #パスワードが違う場合エラー
     def test_different_passwaord(self):
         form_data = {
-            '社員番号' : 111111,
+            "employee_number" : 111111,
             'password' : '222222'
         }
         form = LoginForm(data=form_data)
@@ -47,4 +48,4 @@ class Test_LoginForm(TestCase):
         }
         form = LoginForm(data=form_data)
         #form.is_valid()がTrueの時はログイン成功
-        self.assertTrue(form.is_valid(), "社員番号でエラーが出ませんでした。")
+        self.assertTrue(form.is_valid(), form.errors)
