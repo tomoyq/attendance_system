33e6471bc495fd65f062c5febca53a76b3f945b2
tomoya <arata5.18830@gmail.com>
2024-10-25 17:19:51 +0900

    関数の修正#4

--- work/tests/test_date.py
+++ work/tests/test_date.py
@@ -1,3 +1,4 @@
+import calendar
 import datetime
 
 from django.test import TestCase
@@ -16,13 +17,14 @@ class CalculateDatesTest(TestCase):
 
         #関数に引数を渡してないため今月のすべての日にちを持っているはず
         thisMonthDays = self.date.calculate_days()
-        #タプルを要素にしたリストオブジェクトのため2つ目のindexで月を調べる
-        self.assertEqual(thisMonthDays[0][1], self.date.today.month)
+        #タプルを要素にしたリストオブジェクトのため2つ目のindexで日付を調べる
+        self.assertEqual(thisMonthDays[0][0], 1)
 
         #関数に１月１日のobjを渡すと１月のすべての日にちを持っているはず
         januaryDays = self.date.calculate_days(date=datetime.date(2024, 1, 1))
-        #2つ目のindexに１が入っているはず
-        self.assertEqual(januaryDays[0][1], 1)
+        #monthrangeで月の日数を調べる(戻り値は(曜日の数字, 日数)のタプル)
+        #januaryDaysの要素数が同じになるはず
+        self.assertEqual(len(januaryDays), calendar.monthrange(2024, 1)[1])
 
     def test_calculate_five_months(self):
         month_list = self.date.calculate_five_month()
